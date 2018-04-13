2ededab0af2809a9a68ab7a0088278fb6ec826e4
Kelly Liu <kellyliu@usc.edu>
2018-04-12 19:15:50 -0700

    Message object and SQL INSERT statement for msg data

--- src/messaging/Message.java
+++ src/messaging/Message.java
@@ -1,5 +1,9 @@
 package messaging;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Calendar;
 
 public class Message {
@@ -8,10 +12,43 @@ public class Message {
 	int receiverID; // user receiving message
 	int senderID; // user sending message
 	
+	// Message constructor
 	Message(String message, int sender, int receiver) {
 		this.cal = Calendar.getInstance();
 		this.message = message;
 		this.receiverID = receiver;
 		this.senderID = sender;
+		updateMessages(this);
+	}
+	
+	// add the Message to message database
+	public void updateMessages(Message msg) {
+		Connection conn = null;
+		Statement st = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/messages?user=root&password=root&useSSL=false");
+			st = conn.createStatement();
+			
+			String sqlInsert = "INSERT INTO messagedata " +
+							"VALUES ('" + this.message + "', " + this.cal + ", " + this.senderID + ", " + this.receiverID + ")";
+			st.executeUpdate(sqlInsert);
+		} catch (SQLException sqle) {
+			System.out.println("sqle: " + sqle.getMessage());
+		} catch (ClassNotFoundException cnfe) {
+			System.out.println("cnfe: " + cnfe.getMessage());
+		} 
+		finally {
+			try {
+				if (st != null) {
+					st.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (Exception e) {
+				System.out.println("Close exception: " + e.getMessage());
+			}
+		}
 	}
 }
---
+++ src/messaging/MessageForm.java
@@ -0,0 +1,25 @@
+package messaging;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MessageForm
+ */
+@WebServlet("/MessageForm")
+public class MessageForm extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		
+		String messageText = request.getParameter("text");
+	}
+
+}
