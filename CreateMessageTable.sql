cf0e4d8695164b7ae55abc508f1cddf07530a00b
Kelly Liu <kellyliu@usc.edu>
2018-04-12 19:15:15 -0700

    Updates to create statement

--- CreateMessageTable.sql
+++ CreateMessageTable.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `messagedata` (
-  `messageID` int(11) NOT NULL,
+  `messageID` int(11) NOT NULL AUTO_INCREMENT,
   `messageText` varchar(512) DEFAULT NULL,
   `messageTime` datetime DEFAULT NULL,
   `senderID` int(11) DEFAULT NULL,
-  `userID` int(11) DEFAULT NULL,
+  `receiverID` int(11) DEFAULT NULL,
   PRIMARY KEY (`messageID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
