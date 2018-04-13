54fe14dd7ea62180eea1777ea8c3ef5630f5a180
Kelly Liu <kellyliu@usc.edu>
2018-04-12 18:01:31 -0700

    Message object class

--- src/messaging/Message.java
+++ src/messaging/Message.java
@@ -1,5 +1,17 @@
 package messaging;
 
-public class Message {
+import java.util.Calendar;
 
+public class Message {
+	Calendar cal;
+	String message;
+	int receiverID; // user receiving message
+	int senderID; // user sending message
+	
+	Message(String message, int sender, int receiver) {
+		this.cal = Calendar.getInstance();
+		this.message = message;
+		this.receiverID = receiver;
+		this.senderID = sender;
+	}
 }
