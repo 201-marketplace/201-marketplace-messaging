972c9262c8fdcc2191ffe143fc1ddf76b991cf91
Kelly Liu <kellyliu@usc.edu>
2018-04-12 19:38:02 -0700

    updateMessage method moved to form

--- src/messaging/Message.java
+++ src/messaging/Message.java
@@ -18,37 +18,6 @@ public class Message {
 		this.message = message;
 		this.receiverID = receiver;
 		this.senderID = sender;
-		updateMessages(this);
 	}
 	
-	// add the Message to message database
-	public void updateMessages(Message msg) {
-		Connection conn = null;
-		Statement st = null;
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://localhost/messages?user=root&password=root&useSSL=false");
-			st = conn.createStatement();
-			
-			String sqlInsert = "INSERT INTO messagedata " +
-							"VALUES ('" + this.message + "', " + this.cal + ", " + this.senderID + ", " + this.receiverID + ")";
-			st.executeUpdate(sqlInsert);
-		} catch (SQLException sqle) {
-			System.out.println("sqle: " + sqle.getMessage());
-		} catch (ClassNotFoundException cnfe) {
-			System.out.println("cnfe: " + cnfe.getMessage());
-		} 
-		finally {
-			try {
-				if (st != null) {
-					st.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (Exception e) {
-				System.out.println("Close exception: " + e.getMessage());
-			}
-		}
-	}
 }
--- src/messaging/MessageForm.java
+++ src/messaging/MessageForm.java
@@ -2,6 +2,10 @@ package messaging;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -20,6 +24,41 @@ public class MessageForm extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		
 		String messageText = request.getParameter("text");
+		int receiver = Integer.parseInt(request.getParameter("receiverID"));
+		int sender = Integer.parseInt(request.getParameter("senderID"));
+		Message msg = new Message(messageText, sender, receiver);
+		updateMessages(msg);
+		out.println("Message recorded!");
 	}
-
+	
+	//add Message to message database	
+	protected void updateMessages(Message msg) {
+		Connection conn = null;
+		Statement st = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/messages?user=root&password=root&useSSL=false");
+			st = conn.createStatement();
+			
+			String sqlInsert = "INSERT INTO messagedata " +
+							"VALUES ('" + msg.message + "', " + msg.cal + ", " + msg.senderID + ", " + msg.receiverID + ")";
+			st.executeUpdate(sqlInsert);
+		} catch (SQLException sqle) {
+			System.out.println("sqle: " + sqle.getMessage());
+		} catch (ClassNotFoundException cnfe) {
+			System.out.println("cnfe: " + cnfe.getMessage());
+		} 
+		finally {
+			try {
+				if (st != null) {
+					st.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (Exception e) {
+				System.out.println("Close exception: " + e.getMessage());
+			}
+		}
+	}
 }
