96d30d1499cc5919b69fa7371ff1abf286a65577
Kelly Liu <kellyliu@usc.edu>
2018-04-16 21:32:35 -0700

    Working on websocket

--- src/messaging/MessageFormWS.java
+++ src/messaging/MessageFormWS.java
@@ -1,33 +1,46 @@
 package messaging;
 
-import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Vector;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint(value = "/ws")
 public class MessageFormWS {
 	// static so that it's shared among all ServerSocket instances
-		private static Vector<Session> sessionVec = new Vector<Session>();
+		private static ConcurrentHashMap<String, Session> sessionMap = new ConcurrentHashMap<String, Session>();
 		
 		// The following three methods are automatically called
 		@OnOpen
 		public void open(Session session) {
 			System.out.println("Client connected");
-			sessionVec.add(session);
+			String username = "";
+			sessionMap.put(username, session);
 		}
 		
 		@OnMessage
 		public void onMessage(String message, Session session) {
+			//printing on server-side
 			System.out.println(message);
 			try {
-				for (Session s : sessionVec) {
+				if 
+				// insert message into database
+				
+				// look through sessionMap if other user online
+				if (sessionMap.containsKey(username)) {
+					Session s = sessionMap.get(username);
 					s.getBasicRemote().sendText(message);
 				}
+				
+//				for (Session s : sessionMap) {
+//					s.getBasicRemote().sendText(message);
+//				}
 			} catch (IOException ioe) {
 				System.out.println("ioe: " + ioe.getMessage());
 			}
@@ -36,6 +49,7 @@ public class MessageFormWS {
 		@OnClose
 		public void close(Session session) {
 			System.out.println("Client disconnected");
-			sessionVec.remove(session);
+			String str = "";
+			sessionMap.remove(str, session);
 		}
 }
