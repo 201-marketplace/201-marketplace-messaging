2c9e35806e5775ee20272047b7e12cfb7d0d4c54
Kelly Liu <kellyliu@usc.edu>
2018-04-16 20:32:34 -0700

    Table will take in usernames to identify users:

--- CreateMessageTable.sql
+++ CreateMessageTable.sql
@@ -2,7 +2,8 @@ CREATE TABLE `messagedata` (
   `messageID` int(11) NOT NULL AUTO_INCREMENT,
   `messageText` varchar(512) DEFAULT NULL,
   `messageTime` datetime DEFAULT NULL,
-  `senderID` int(11) DEFAULT NULL,
-  `receiverID` int(11) DEFAULT NULL,
+  `senderID` varchar(30) DEFAULT NULL,
+  `receiverID` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`messageID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
